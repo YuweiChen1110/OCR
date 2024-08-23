369319b79e875d46bc931ceedb91a9cb4f31edca
Yuwei Chen <yuwei.chen@intel.com>
2024-08-23 13:36:13 +0800

    Modify PaddleOCR download
    Signed-off-by: Yuwei Chen <yuwei.chen@intel.com>

--- PaddleOCR/st.py
+++ PaddleOCR/st.py
@@ -2,63 +2,9 @@ import streamlit as st
 from paddleocr import PaddleOCR
 import uuid
 import os
-import shutil
-import cv2
-import urllib.request
-import tarfile
 import base64
-from tempfile import NamedTemporaryFile
-
-# Clean the cache to avoid corrupted files issue
-cache_dir = os.path.expanduser('~/.paddleocr')
-
-def remove_readonly(func, path, _):
-    "Clear the readonly bit and reattempt the removal"
-    os.chmod(path, 0o777)
-    func(path)
-
-if os.path.exists(cache_dir):
-    try:
-        shutil.rmtree(cache_dir, onerror=remove_readonly)
-    except PermissionError as e:
-        st.warning(f"PermissionError encountered while deleting cache directory: {e}")
-
-# Model download links
-model_urls = {
-    'det': 'https://paddleocr.bj.bcebos.com/dygraph_v2.0/en/en_ppocr_server_v2.0_det_infer.tar',
-    'rec': 'https://paddleocr.bj.bcebos.com/dygraph_v2.0/en/en_ppocr_server_v2.0_rec_infer.tar',
-    'cls': 'https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_cls_infer.tar'
-}
-
-# Download and extract models
-def download_and_extract(url, output_dir):
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-    filename = url.split('/')[-1]
-    filepath = os.path.join(output_dir, filename)
-    
-    if not os.path.exists(filepath):
-        print(f"Downloading {filename}...")
-        urllib.request.urlretrieve(url, filepath)
-        print(f"Downloaded {filename}")
-
-    print(f"Extracting {filename}...")
-    with tarfile.open(filepath, 'r') as tar:
-        tar.extractall(path=output_dir)
-    print(f"Extracted {filename}")
-
-# Check and download models
-def check_and_download_models():
-    model_dirs = {
-        'det': 'ch_ppstructure_mobile_v2.0_SLANet_infer',
-        'rec': 'models/rec/en_PP-OCRv4_rec_infer'
-    }
-    for key, model_dir in model_dirs.items():
-        if not os.path.exists(model_dir):
-            download_and_extract(model_urls[key], model_dir)
-
-# Perform model check and download
-check_and_download_models()
+import cv2
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 
 # Global variable for caching the OCR model
 ocr_model_cache = None
@@ -70,14 +16,9 @@ def load_ocr_model():
     if ocr_model_cache is not None:
         return ocr_model_cache
     
-    det_model_dir = 'models/det/ch_ppstructure_mobile_v2.0_SLANet_infer'
-    rec_model_dir = 'models/rec/en_PP-OCRv4_rec_infer'
-    
     ocr_model_cache = PaddleOCR(
         use_angle_cls=True, 
-        lang='en', 
-        det_model_dir=det_model_dir, 
-        rec_model_dir=rec_model_dir
+        lang='en'
     )
     return ocr_model_cache
 
@@ -162,54 +103,50 @@ load_ocr_model()
 # File uploader
 uploaded_file = st.sidebar.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
-    unique_filename = str(uuid.uuid4()) + '.jpg'
-    with open(unique_filename, 'wb') as f:
-        f.write(bytes_data)
-    st.image(bytes_data, caption='Uploaded Image.', use_column_width=True)
+    with TemporaryDirectory() as temp_dir:
+        bytes_data = uploaded_file.getvalue()
+        unique_filename = os.path.join(temp_dir, str(uuid.uuid4()) + '.jpg')
+        with open(unique_filename, 'wb') as f:
+            f.write(bytes_data)
+        st.image(bytes_data, caption='Uploaded Image.', use_column_width=True)
 
-    # Preprocess image
-    binary_image = preprocess_image(unique_filename)
-    processed_image_path = unique_filename.replace('.jpg', '_processed.jpg')
-    cv2.imwrite(processed_image_path, binary_image)
-    
-    # Run inference
-    st.write("Recognizing text from image...")
-    try:
-        raw_results = inference(processed_image_path)
-        
-        with st.expander("Raw OCR results"):
-            st.write("Raw OCR results:", raw_results)
+        # Preprocess image
+        binary_image = preprocess_image(unique_filename)
+        processed_image_path = unique_filename.replace('.jpg', '_processed.jpg')
+        cv2.imwrite(processed_image_path, binary_image)
+
+        # Run inference
+        st.write("Recognizing text from image...")
+        try:
+            raw_results = inference(processed_image_path)
             sorted_text_info = extract_sorted_text(raw_results)
+            paragraphs = merge_lines_to_paragraphs(sorted_text_info)
             
-            st.write("Sorted text:")
-            for line in sorted_text_info:
-                st.write(f"Detected text: {line[1]} (Confidence score: {line[2]})")
-        
-        # Merge lines into paragraphs
-        paragraphs = merge_lines_to_paragraphs(sorted_text_info)
-        
-        st.write("Paragraphs:")
-        for paragraph in paragraphs:
-            st.write(paragraph)
-        
-        # User input for file names in sidebar
-        paragraph_file_name = st.sidebar.text_input("Enter the file name for recognized text download:", "recognized_text.md")
-        
-        # Create download link in sidebar
-        if st.sidebar.button("Generate recognized text as markdown"):
-            with st.spinner('Processing...'):
-                with NamedTemporaryFile(delete=False, suffix=".md") as tmp_file:
-                    convert_paragraphs_to_markdown(paragraphs, tmp_file.name)
-                    tmp_file.flush()
-                    tmp_file.seek(0)
-                    href = create_download_link(tmp_file.name, paragraph_file_name)
-                    st.sidebar.markdown(href, unsafe_allow_html=True)
-        
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
-    finally:
-        if os.path.exists(unique_filename):
-            os.remove(unique_filename)
-        if os.path.exists(processed_image_path):
-            os.remove(processed_image_path)
+            with st.expander("Raw OCR results"):
+                for line in sorted_text_info:
+                    st.write(f"Detected text: {line[1]} (Confidence score: {line[2]})")
+
+            st.write("Paragraphs:")
+            for paragraph in paragraphs:
+                st.write(paragraph)
+
+            # User input for file names in sidebar
+            paragraph_file_name = st.sidebar.text_input("Enter the file name for recognized text download:", "recognized_text.md")
+            
+            # Create download link in sidebar
+            if st.sidebar.button("Generate recognized text as markdown"):
+                with st.spinner('Processing...'):
+                    with NamedTemporaryFile(delete=False, suffix=".md", dir=temp_dir) as tmp_file:
+                        convert_paragraphs_to_markdown(paragraphs, tmp_file.name)
+                        tmp_file.flush()
+                        tmp_file.seek(0)
+                        href = create_download_link(tmp_file.name, paragraph_file_name)
+                        st.sidebar.markdown(href, unsafe_allow_html=True)
+
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+        finally:
+            if os.path.exists(unique_filename):
+                os.remove(unique_filename)
+            if os.path.exists(processed_image_path):
+                os.remove(processed_image_path)
